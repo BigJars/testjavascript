d2aaef3cfc4dd23b2a3d029ab2bd3f6ffce4c44c
BigJars <58695304+BigJars@users.noreply.github.com>
2019-12-17 08:50:21 -0500

    Add files via upload

--- JavaScript lab 1/Lab1.js
+++ JavaScript lab 1/Lab1.js
@@ -1,4 +1,4 @@
-var numberOfApples = 10;
+var numberOfApples = 11;
 document.write("The number of apples I have is " + numberOfApples + "<br>");
 numberOfApples++;
 document.write("The number of apples I have is " + numberOfApples + "<br>");
@@ -14,7 +14,7 @@ numberOfApples++;
 
 document.write("<br><br>")
 
-var numberOfNinja = 100;
+var numberOfNinja = 200;
 document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
 document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
 document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
